8454dad11f52bce67a4a574c686ebe6f741250c4
Matthias Winkler <mwinkler@gmx.ch>
2026-01-05 20:23:44 +0100

    Update test

--- test.ts
+++ test.ts
@@ -1,11 +1,11 @@
 m5scroll.onEncoderChange(function (value, delta) {
-    serial.writeLine("v: " + value + " d: " + delta)
+    serial.writeLine("v: " + value + ", d: " + delta)
 })
 m5scroll.onButton(function (pressed) {
     if (pressed) {
-        m5scroll.setLEDColor(0xff0000)
-    } else {
         m5scroll.setLEDColor(0x00ff00)
+    } else {
+        m5scroll.setLEDColor(0x00ffff)
     }
 })
-m5scroll.turnLEDOff()
+m5scroll.setLEDColor(0xff0000)
